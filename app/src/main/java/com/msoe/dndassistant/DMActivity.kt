5afcec2dbe79d0c3840bfa347cf4f57695377f31
NZawarus <zawarusn@msoe.edu>
2025-04-15 14:01:30 -0500

    Connected DM and Player Activities

---
+++ app/src/main/java/com/msoe/dndassistant/DMActivity.kt
@@ -0,0 +1,11 @@
+package com.msoe.dndassistant
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DMActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dm)
+    }
+}
--- app/src/main/java/com/msoe/dndassistant/MainActivity.kt
+++ app/src/main/java/com/msoe/dndassistant/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.msoe.dndassistant
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val dmButton = findViewById<Button>(R.id.btnHost)
+        val playerButton = findViewById<Button>(R.id.btnJoin)
+
+        dmButton.setOnClickListener {
+            val intent = Intent(this, DMActivity::class.java)
+            startActivity(intent)
+        }
+
+        playerButton.setOnClickListener {
+            val intent = Intent(this, PlayerActivity::class.java)
+            startActivity(intent)
         }
     }
 }
